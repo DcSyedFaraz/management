a49e0cce7dbddeeedd156e0fa8bdac5351a54129
Syed Faraz Shahid <135306876+DcSyedFaraz@users.noreply.github.com>
2025-07-11 18:36:11 +0500

    Allow editing all order fields

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -27,7 +27,39 @@ class AdminOrderController extends Controller
             'status' => ['nullable', 'string'],
             'last_dispatch' => ['nullable', 'date'],
             'residence' => ['nullable', 'string'],
+            'reuseable_bed_protection' => ['nullable', 'boolean'],
+            'beantrager' => ['nullable', 'string'],
+            'sign' => ['nullable', 'string'],
+            'geburtsdatum' => ['nullable', 'string'],
+            'versicherter' => ['nullable'],
+            'address' => ['nullable'],
+            'antragsteller' => ['nullable'],
+            'insuranceType' => ['nullable', 'string'],
+            'insuranceProvider' => ['nullable', 'string'],
+            'insuranceNumber' => ['nullable', 'string'],
+            'pflegegrad' => ['nullable', 'string'],
+            'changeProvider' => ['nullable', 'boolean'],
+            'requestBedPads' => ['nullable', 'boolean'],
+            'isSameAsContact' => ['nullable', 'boolean'],
+            'deliveryAddress' => ['nullable', 'string'],
+            'applicationReceipt' => ['nullable', 'string'],
+            'awarenessSource' => ['nullable', 'string'],
+            'consultation_check' => ['nullable', 'integer'],
+            'products' => ['nullable'],
+            'dispatch_months' => ['nullable'],
         ]);
+
+        $data['changeProvider'] = $request->boolean('changeProvider');
+        $data['requestBedPads'] = $request->boolean('requestBedPads');
+        $data['reuseable_bed_protection'] = $request->boolean('reuseable_bed_protection');
+        $data['isSameAsContact'] = $request->boolean('isSameAsContact');
+
+        foreach (['versicherter', 'address', 'antragsteller', 'products', 'dispatch_months'] as $jsonField) {
+            if ($request->filled($jsonField)) {
+                $data[$jsonField] = json_decode($request->input($jsonField), true) ?? [];
+            }
+        }
+
         $order->update($data);
 
         return redirect()->route('orders.edit', $order->id)
